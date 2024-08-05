3a9561590553c8be13d752840586f35dd3576c22
Tony Wang <tiancheng_wang@brown.edu>
2024-08-04 21:52:24 -0700

    revision

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ REFRESH_INTERVAL_RANGE_END_SECOND = 350
 # TEST_REFRESH_INTERVAL_RANGE_END_SECOND = 10
 
 def isInDesiredMonthYear(availableDate: str) -> bool:
-    desiredMonthYear = ['June, 2024', 'July, 2024', 'August, 2024', 'September, 2024', 'October, 2024']
+    desiredMonthYear = ['July, 2024', 'August, 2024']
     return sum([desired in availableDate for desired in desiredMonthYear]) > 0
 
 def doLogIn(driver):
@@ -43,29 +43,33 @@ def checkEarliestVancAvailability(driver) -> bool:
     earliestVancouverSlot = driver.find_element(by=By.XPATH, value='//*[@id="paymentOptions"]/div[2]/table/tbody/tr[4]/td[2]').text
     return isInDesiredMonthYear(earliestVancouverSlot)
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(10)
-driver.maximize_window()
+i=0
+while True:
+    try:
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(10)
+        driver.maximize_window()
 
-driver.get("https://ais.usvisa-info.com/en-ca/niv/users/sign_in")
+        driver.get("https://ais.usvisa-info.com/en-ca/niv/users/sign_in")
 
-doLogIn(driver)
-
-# only land on this page for the first time
-continueButton = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div[2]/div[1]/div/div/div[1]/div[2]/ul/li/a')
-continueButton.click()
-
-try:
-    while True:
-        clickPayVisaFee(driver)
-        sentBackToLoginPage = False
-        while not sentBackToLoginPage:
-            if checkEarliestVancAvailability(driver):
-                EmailNotificationService.sendSuccessEmail()
-                exit()
-            time.sleep(randrange(REFRESH_INTERVAL_RANGE_START_SECOND, REFRESH_INTERVAL_RANGE_END_SECOND))
-            driver.refresh()
-            sentBackToLoginPage = checkIsLoginPage(driver)
         doLogIn(driver)
-except:
-    EmailNotificationService.sendCrashEmail()
+
+        # only land on this page for the first time
+        continueButton = driver.find_element(by=By.XPATH, value='//*[@id="main"]/div[2]/div[2]/div[1]/div/div/div[1]/div[2]/ul/li/a')
+        continueButton.click()
+
+        while True:
+            clickPayVisaFee(driver)
+            sentBackToLoginPage = False
+            while not sentBackToLoginPage:
+                print("scan" + str(i))
+                i+=1
+                if checkEarliestVancAvailability(driver):
+                    EmailNotificationService.sendSuccessEmail()
+                    exit()
+                time.sleep(randrange(REFRESH_INTERVAL_RANGE_START_SECOND, REFRESH_INTERVAL_RANGE_END_SECOND))
+                driver.refresh()
+                sentBackToLoginPage = checkIsLoginPage(driver)
+            doLogIn(driver)
+    except:
+        print("crashed, reopen chrome...")
